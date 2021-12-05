bf7318bde2ca2c0428a2fc1af803c01a7fa2f4bb
SNVZero <simakov_nikita01@mail.ru>
2021-12-05 12:55:47 +0300

    why so s

---
+++ js/code.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	$('.slider').slick({
+
+		slidesToShow:6,
+		autoplay:true,
+		speed:2000,
+		autoplaySpeed:1000,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:4
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+
+	$("#slider").owlCarousel({
+	items: 1,
+	loop: true,
+	nav: true,
+	dots: false,
+	smartSpeed: 700,
+	animateOut:  'slideOutDown' ,
+	animateIn:  'slideOutDown' ,
+	slideSpeed : 1000,
+    singleItem : true,
+
+
+	});
+
+
+	$('.reviews__slider').on(`init reInit`, function (event, slick) {
+        $('.slick-slide-num-current').text('0' + 1 + ' '); // ' / ' + '0' + slick.slideCount
+    })
+    $('.reviews__slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+        $('.slick-slide-num-current').text('0' + (currentSlide + 1) + ' '); // + ' / 0' + slick.slideCount
+    })
+    $('.reviews__slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        adaptiveHeight: true,
+        easing: 'ease',
+        appendArrows: '.slick__arrows',
+        prevArrow: '<span class="slick-prev"><</span>',
+        nextArrow: '<span class="slick-next">></span>'
+
+    });
+
+	$('.partners__slider-1').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        easing: 'ease',
+        autoplay: true,
+        pauseOnFocus: true,
+        autoplaySpeed: 2500,
+        speed: 1500,
+    });
+    $('.partners__slider-2').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        easing: 'ease',
+        autoplay: true,
+        pauseOnFocus: true,
+        autoplaySpeed: 2700,
+        speed: 2000,
+    });
+});
