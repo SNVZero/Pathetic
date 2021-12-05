d0ba950289ed36b67277408dd7451f1da20cc3f0
SNVZero <simakov_nikita01@mail.ru>
2021-12-05 13:18:50 +0300

    OOOle

--- js/code.js
+++ js/code.js
@@ -78,4 +78,27 @@ $(document).ready(function(){
         autoplaySpeed: 2700,
         speed: 2000,
     });
+    (function(){
+        const header=document.querySelector('.header');
+        window.onscroll= () => {
+            if(window.pageYOffset>50){
+                header.classList.add('header_active');
+            }
+            else{
+                header.classList.remove('header_active');
+            }
+        };
+    }());
+
+    (function(){
+    const burgerItem=document.querySelector('.burger');
+    const menu=document.querySelector('.header_nav');
+    const menuCloseItem=document.querySelector('.header_nav-close');
+    burgerItem.addEventListener('click',()=>{
+    menu.classList.add('header_nav_active');
+    });
+    menuCloseItem.addEventListener('click',()=>{
+        menu.classList.remove('header_nav_active');
+        });
+    }());
 });
