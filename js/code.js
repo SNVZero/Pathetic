7f470209d3c0f1447cbfdbfb44155fcb098a821b
z-mirkhel <91228790+z-mirkhel@users.noreply.github.com>
2022-01-22 17:37:01 +0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -109,7 +109,6 @@ $('.partners__slider-2').slick({
     ]
 });
 
-
 $('.faq__title').click(function(event) {
     if($('.faq-field').hasClass('one')){
         $('.faq__title').not($(this)).parent().removeClass('active');
